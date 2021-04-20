0dbee7727adbf0fe3ff793d4a71daf960534d541
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-04-19 22:42:00 -0500

    addded navigation arrows

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CreateUser from './components/createUser';
 import CreateExercise from './components/createExercise';
 import GetExerciseLog from './components/getExerciseLog';
 import React, { useState } from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleLeft, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  const [activeWindow, setActiveWindow] = useState(0);
+  const [activeWindowCounter, setActiveWindowCounter] = useState(0);
+  let windowArray = [<CreateUser/>, <CreateExercise/>, <GetExerciseLog/>]
 
-  // things to fix: add component navigation keys
+  const handleNavigation = e => {
+    let navigateAction;
+    
+    if (e.target.className === 'nextButton' || e.target.parentNode.className === 'nextButton' || e.target.parentNode.parentNode.className === 'nextButton') {
+      navigateAction = 'next';
+    } else if (e.target.className === 'previousButton' || e.target.parentNode.className === 'previousButton' || e.target.parentNode.parentNode.className === 'previousButton') {
+      navigateAction = 'previous';
+    }
+    
+    if ((activeWindowCounter === 0 && navigateAction === 'previous') || (activeWindowCounter >= 2 && navigateAction === 'next')) {
+      // do nothing
+    } else {
+      navigateAction === 'previous' ? setActiveWindowCounter(activeWindowCounter - 1) : setActiveWindowCounter(activeWindowCounter + 1)
+    }
+  }
 
   return (
     <div className="AppContainer">
@@ -19,10 +34,26 @@ function App() {
           <h1>Exercise Tracker</h1>
           <p>By: Christian Vega-Munguia 💪</p>
         </div>
+        <div className="navigationArrowsContainer">
+          {activeWindowCounter > 0 && 
+          <div onClick={handleNavigation} className="previousButton">
+            <FontAwesomeIcon
+              icon={faArrowAltCircleLeft}
+              size="3x"
+              />
+          </div>
+          }
+          {activeWindowCounter < 2 &&
+          <div onClick={handleNavigation} className="nextButton">
+            <FontAwesomeIcon
+              icon={faArrowAltCircleRight}
+              size="3x"
+            />
+          </div>
+          }
+        </div>
         <div className="componentContainer">
-          <CreateUser/>
-          <CreateExercise/>
-          <GetExerciseLog/>
+          {windowArray[activeWindowCounter]}
         </div>
       </div>
     </div>
