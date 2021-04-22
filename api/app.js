0759b6cd99bc3e4a501fb412cd1dd0f6e8af86f7
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-04-22 00:06:55 -0500

    added heroku stuff

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/build')));
 
 app.use('/', indexRouter);
 app.use('/api/exercise', exerciseRouter);
