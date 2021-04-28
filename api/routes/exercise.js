5f3c7694a5b976d7c36c0a623fcb7b9f1c2f69b5
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-04-28 00:17:56 -0500

    fixed log path

--- api/routes/exercise.js
+++ api/routes/exercise.js
@@ -20,6 +20,59 @@ router.get('/', function(req, res, next) {
 
 });
 
+router.post('/users', function(req, res, next) {
+  let username = req.body.username;
+  
+  async function checkIfUserExists () {
+    console.log('Checking if User exists in DB...')
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    const asyncUserFinder = await UserModel.find({'username': username});
+    mongoose.connection.close();
+    return asyncUserFinder
+  }
+  
+  async function createUser () {
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    const newUser = new UserModel({
+      'username': username,
+      count: 0,
+      log: []
+    });
+    let userId;
+    
+    console.log('New user Model has been created successfully.');
+    console.log(newUser);
+    
+    newUser.save()
+    .then((user, err) => {
+      userId = user._id;
+      const userMinified = {'_id': userId, 'username': username}
+      console.log('User was saved!');
+      mongoose.connection.close();
+      res.send(userMinified);
+    })
+    .catch(err => {
+      console.log('uh oh. error while saving:');
+      res.send('error');
+      console.error(err);
+      mongoose.connection.close();
+    })
+  }
+  
+  checkIfUserExists()
+  .then((userFoundInDB) => {
+    let userAlreadyExists = Object.keys(userFoundInDB).length;
+    if (userAlreadyExists) {
+      let existingUserRecord = {'_id': userFoundInDB[0]['_id'], 'username': userFoundInDB[0]['username']};
+      console.log(`This user already exists in the DB: ${existingUserRecord}`);
+      res.send(existingUserRecord);
+    } else {
+      console.log(`User not found. Creating new user...`)
+      createUser();
+    }
+  })
+});
+
 router.get('/users', function(req, res, next) {
 
   async function getAllUsers () {
@@ -38,9 +91,75 @@ router.get('/users', function(req, res, next) {
   });
 });
 
-router.get('/log/:userId?', function(req, res, next) {
-  console.log(req.query)
-  let userId = req.query.userId;
+
+router.post('/users/:_id/exercises', function(req, res, next) {
+  let userId = new String(req.path).split('/')[2];
+  let description = req.body.description;
+  let duration = parseInt(req.body.duration);
+  let date = DateTime.fromISO(req.body.date);
+  let newExerciseRecordLog = {'_id': userId, 'description': description, 'duration': duration, 'date': ''}
+  
+  if (userId && description && duration) {
+    console.log('All required parameters are present.');
+  } else {
+    console.log('Missing one or more required parameters');
+    res.status(400);
+    res.send('Please submit request again with required parameters: userId, description and duration');
+    return
+  }
+
+  console.log('Validating date...');
+  if (date.toISODate() && date.toISODate() <= DateTime.now().toISODate()) {
+    console.log(`${date} is a valid date`);
+    console.log(date.toISODate())
+    newExerciseRecordLog.date = date.toISODate();
+  } else {
+    console.log(`Date was either undefined or not a valid date. Using today's date: ${DateTime.now().toISODate()}`)
+    newExerciseRecordLog.date = DateTime.now().toISODate();
+  }
+
+  console.log(`Exercise record to be added in log:`)
+  console.log(newExerciseRecordLog);
+
+  async function checkIfUserExists () {
+    console.log('Opening MongoDB Atlas connection...')
+    console.log('Checking if User exists in DB...')
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    const asyncUserFinder = await UserModel.findOne({'_id': userId});
+    return asyncUserFinder
+  }
+  
+  async function run () {
+    const userFoundInDB = await checkIfUserExists();
+    let userExists = Object.keys(userFoundInDB).length;
+    if (userExists) {
+      console.log(`User record found in the DB.`);
+      console.log('adding new exercise record to log...')
+      try {
+        userFoundInDB.log.push(newExerciseRecordLog);
+        userFoundInDB.count = userFoundInDB.log.length;
+        // console.log(userFoundInDB.log);
+        await userFoundInDB.save();
+        let responseWithUsername = newExerciseRecordLog;
+        responseWithUsername.username = userFoundInDB.username;
+        res.send(responseWithUsername);
+        console.log('Closing MongoDB connection...')
+        await mongoose.connection.close();
+        return 
+      } catch (err) {
+        console.error('unexpected error encontered.')
+        console.error(err);
+      }
+    } else {
+      res.send(`userId: ${userId} was not found in the database!`);
+    }
+  }
+
+  run();
+});
+
+router.get('/users/:_id/logs', function(req, res, next) {
+  let userId = new String(req.path).split('/')[2];
   let from = DateTime.fromISO(req.query.from);
   let to = DateTime.fromISO(req.query.to);
   let limit = Infinity;
@@ -106,124 +225,6 @@ router.get('/log/:userId?', function(req, res, next) {
   })
 });
 
-router.post('/users', function(req, res, next) {
-  let username = req.body.username;
-  
-  async function checkIfUserExists () {
-    console.log('Checking if User exists in DB...')
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    const asyncUserFinder = await UserModel.find({'username': username});
-    mongoose.connection.close();
-    return asyncUserFinder
-  }
-  
-  async function createUser () {
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    const newUser = new UserModel({
-      'username': username,
-      count: 0,
-      log: []
-    });
-    let userId;
-    
-    console.log('New user Model has been created successfully.');
-    console.log(newUser);
-    
-    newUser.save()
-    .then((user, err) => {
-      userId = user._id;
-      const userMinified = {'_id': userId, 'username': username}
-      console.log('User was saved!');
-      mongoose.connection.close();
-      res.send(userMinified);
-    })
-    .catch(err => {
-      console.log('uh oh. error while saving:');
-      res.send('error');
-      console.error(err);
-      mongoose.connection.close();
-    })
-  }
-  
-  checkIfUserExists()
-  .then((userFoundInDB) => {
-    let userAlreadyExists = Object.keys(userFoundInDB).length;
-    if (userAlreadyExists) {
-      let existingUserRecord = {'_id': userFoundInDB[0]['_id'], 'username': userFoundInDB[0]['username']};
-      console.log(`This user already exists in the DB: ${existingUserRecord}`);
-      res.send(existingUserRecord);
-    } else {
-      console.log(`User not found. Creating new user...`)
-      createUser();
-    }
-  })
-});
-
-router.post('/add', function(req, res, next) {
-  let userId = req.body.userId;
-  let description = req.body.description;
-  let duration = parseInt(req.body.duration);
-  let date = DateTime.fromISO(req.body.date);
-  let newExerciseRecordLog = {'_id': userId, 'description': description, 'duration': duration, 'date': ''}
-  
-  if (userId && description && duration) {
-    console.log('All required parameters are present.');
-  } else {
-    console.log('Missing one or more required parameters');
-    res.status(400);
-    res.send('Please submit request again with required parameters: userId, description and duration');
-    return
-  }
-
-  console.log('Validating date...');
-  if (date.toISODate() && date.toISODate() <= DateTime.now().toISODate()) {
-    console.log(`${date} is a valid date`);
-    console.log(date.toISODate())
-    newExerciseRecordLog.date = date.toISODate();
-  } else {
-    console.log(`Date was either undefined or not a valid date. Using today's date: ${DateTime.now().toISODate()}`)
-    newExerciseRecordLog.date = DateTime.now().toISODate();
-  }
-
-  console.log(`Exercise record to be added in log:`)
-  console.log(newExerciseRecordLog);
-
-  async function checkIfUserExists () {
-    console.log('Opening MongoDB Atlas connection...')
-    console.log('Checking if User exists in DB...')
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    const asyncUserFinder = await UserModel.findOne({'_id': userId});
-    return asyncUserFinder
-  }
-  
-  async function run () {
-    const userFoundInDB = await checkIfUserExists();
-    let userExists = Object.keys(userFoundInDB).length;
-    if (userExists) {
-      console.log(`User record found in the DB.`);
-      console.log('adding new exercise record to log...')
-      try {
-        userFoundInDB.log.push(newExerciseRecordLog);
-        userFoundInDB.count = userFoundInDB.log.length;
-        // console.log(userFoundInDB.log);
-        await userFoundInDB.save();
-        let responseWithUsername = newExerciseRecordLog;
-        responseWithUsername.username = userFoundInDB.username;
-        res.send(responseWithUsername);
-        console.log('Closing MongoDB connection...')
-        await mongoose.connection.close();
-        return 
-      } catch (err) {
-        console.error('unexpected error encontered.')
-        console.error(err);
-      }
-    } else {
-      res.send(`userId: ${userId} was not found in the database!`);
-    }
-  }
-
-  run();
-});
 
 
 
